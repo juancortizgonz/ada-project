6cddcd3b3fc408eb0c79d208d077fa9a95e97ddf
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2023-01-20 17:29:40 -0500

    Exec query manager initial structure added

--- disjoint_set.py
+++ disjoint_set.py
@@ -1,3 +1,4 @@
+'''
 from collections import defaultdict
 
 """ 
@@ -43,12 +44,86 @@ class ListSet:
 # https://www.geeksforgeeks.org/linked-list-representation-disjoint-set-data-structures/
 def main():
     first_test = ListSet()
+
     first_test.make_set(12)
     first_test.make_set(4)
     first_test.make_set(22)
 
     print(f"find(13): {first_test.find_element(12)}")
 
+
+if __name__ == "__main__":
+    main()
+
+    '''
+
+
+from collections import defaultdict
+
+class Node:
+    def __init__(self, val, item_ptr):
+        self.val = val
+        self.item_ptr = item_ptr
+        self.next = None
+
+class Item:
+    def __init__(self, hd, tl):
+        self.hd = hd
+        self.tl = tl
+
+class ListSet:
+    def __init__(self):
+        self.node_address = defaultdict(lambda: None)
+
+    def makeset(self, a):
+        new_set = Item(Node(a, None), None)
+        new_set.hd.item_ptr = new_set
+        new_set.tl = new_set.hd
+        self.node_address[a] = new_set.hd
+
+    def find(self, key):
+        node = self.node_address[key]
+        return node.item_ptr
+
+    def union(self, i1, i2):
+        cur = i2.hd
+        while cur:
+            cur.item_ptr = i1
+            cur = cur.next
+        i1.tl.next = i2.hd
+        i1.tl = i2.tl
+        del i2
+
+def execOperation(queryOpt, x, y=''):
+    if y == '':
+        return queryOpt == x
+    else:
+        if queryOpt == 1:
+            print("Query 1")
+        elif queryOpt == 2:
+            print("Query 2")
+        elif queryOpt == 3:
+            print("Query 3")
+        else:
+            print("Query not defined")
+
+def main():
+    execOperation(3, 3)
+    a = ListSet()
+    a.makeset(13)
+    a.makeset(25)
+    a.makeset(45)
+    a.makeset(65)
+
+    print(f"find(13): {a.find(13)}")
+    print(f"find(25): {a.find(25)}")
+    print(f"find(65): {a.find(65)}")
+    print(f"find(45): {a.find(45)}")
+    print()
+    print("Union(find(65), find(45))")
+    a.union(a.find(65), a.find(45))
+    print(f"find(65): {a.find(65)}")
+    print(f"find(45): {a.find(45)}")
 
 if __name__ == "__main__":
     main()
